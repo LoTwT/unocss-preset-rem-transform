1d41cbc05327966a890f3ced8aa8565a8cc1b7d0
LoTwT <709937065@qq.com>
2022-08-29 15:33:04 +0800

    feat: unocss-preset-rem-transform

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,27 @@
-export const one = 1
+import type { Preset } from "unocss"
+
+const remRE = /^-?[\.\d]+rem$/
+
+export interface RemTransformOptions {
+  transform?: number | ((value: number) => number)
+}
+
+export default function presetRemTransform(
+  options: RemTransformOptions = {},
+): Preset {
+  const { transform = 1 } = options
+
+  return {
+    name: "unocss-preset-rem-transform",
+    postprocess: (util) => {
+      util.entries.forEach((i) => {
+        const value = i[1]
+        if (value && typeof value === "string" && remRE.test(value))
+          i[1] =
+            typeof transform === "function"
+              ? `${transform(+value.slice(0, -3))}rem`
+              : `${+value.slice(0, -3) * transform}rem`
+      })
+    },
+  }
+}
