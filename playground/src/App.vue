fcf3856e95e19c134a675644a42e5897b382c60b
LoTwT <709937065@qq.com>
2022-08-29 15:45:27 +0800

    chore: add playground

---
+++ playground/src/App.vue
@@ -0,0 +1,7 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div p-3 w-10 text-center bg-blue-500>unocss-preset-rem-transform</div>
+</template>
+
+<style scoped></style>
---
+++ playground/unocss.config.ts
@@ -0,0 +1,34 @@
+import {
+  defineConfig,
+  presetAttributify,
+  presetIcons,
+  presetTypography,
+  presetUno,
+  transformerDirectives,
+  transformerVariantGroup,
+} from "unocss"
+import presetRemTransform from "unocss-preset-rem-transform"
+
+export default defineConfig({
+  shortcuts: [
+    [
+      "btn",
+      "border border-rounded border-blue-600 px-4 py-2 w-full rounded inline-block bg-blue-600 text-white cursor-pointer hover:bg-blue-800 disabled:cursor-not-allowed disabled:bg-gray-600 disabled:border-gray-700 disabled:opacity-50",
+    ],
+  ],
+  presets: [
+    presetUno(),
+    presetAttributify(),
+    presetIcons({
+      scale: 1.2,
+      warn: true,
+    }),
+    presetTypography(),
+    // transform is number
+    // presetRemTransform({ transform: 4 }),
+    // transform is function
+    presetRemTransform({ transform: (value) => value * 8 }),
+  ],
+  transformers: [transformerDirectives(), transformerVariantGroup()],
+  safelist: "prose prose-sm m-auto text-left".split(" "),
+})
