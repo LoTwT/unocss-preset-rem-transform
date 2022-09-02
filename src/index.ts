5a3361ca81545a729d19ea31f8c2b8b4dfe74d7a
LoTwT <709937065@qq.com>
2022-09-03 00:56:59 +0800

    feat: remove verbose property divisor

--- src/index.ts
+++ src/index.ts
@@ -4,17 +4,14 @@ const remRE = /^-?[\.\d]+rem$/
 
 export interface RemTransformOptions {
   multiplier: number
-  divisor: number
 }
 
 export default function presetRemTransform(
   options: Partial<RemTransformOptions>,
 ): Preset {
-  const { multiplier = 1, divisor = 1 } = options
+  const { multiplier = 1 } = options
 
-  if (multiplier < 0) throw new Error("multiplier must > 0")
-
-  if (divisor < 0) throw new Error("divisor must > 0")
+  if (multiplier <= 0) throw new Error("multiplier must > 0")
 
   return {
     name: "unocss-preset-rem-transform",
@@ -22,7 +19,7 @@ export default function presetRemTransform(
       util.entries.forEach((i) => {
         const value = i[1]
         if (value && typeof value === "string" && remRE.test(value))
-          i[1] = `${(+value.slice(0, -3) * multiplier) / divisor}rem`
+          i[1] = `${+value.slice(0, -3) * multiplier}rem`
       })
     },
   }
